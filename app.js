762ee4c89b070ae24ab81ea94bd257af56337889
Umair Yasin <umairyasin226@gmail.com>
2023-10-12 22:41:16 +0500

    cors setting

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json()); //Use first
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'process.env.FRONTEND_URL',
+    origin: 'http://localhost:5173/',
     methods: ["GET","POST", "PUT", "DELETE"],
     credentials: true,
 }));
