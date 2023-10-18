31380159e5c5a4eef9e635e66174c9e093e5710b
Umair Yasin <umairyasin226@gmail.com>
2023-10-18 10:10:10 +0500

    added front end env

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 
 const corsOptions ={
-    origin:"http://localhost:5173",
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET","POST", "PUT", "DELETE"],
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
