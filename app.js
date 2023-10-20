2d0c68ab8fd68caf32bfc9f28148a8ab0cb81909
Umair Yasin <umairyasin226@gmail.com>
2023-10-20 10:42:22 +0500

    added link of frontend

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ app.use((req, res, next) => {
   });
   
 
-const corsOptions ={
-    origin: "https://todo-app-frontend-iota.vercel.app",
-    methods: ["GET","POST", "PUT", "DELETE"],
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin: "https://todo-app-frontend-iota.vercel.app",
+//     methods: ["GET","POST", "PUT", "DELETE"],
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200,
+// }
+// app.use(cors(corsOptions));
 //custom url /users is added and it will be present in every url
 //using routes
 app.use("/api/v1/users", userRouter) 
